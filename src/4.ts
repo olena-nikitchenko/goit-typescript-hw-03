50299487b92aa345a038ff77f61706c4d600bac5
olena-nikitchenko <helnikitka@gmail.com>
2023-10-23 21:44:09 +0300

    solution_task4

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,54 @@
-const key = new Key();
+class Key {
+	private signature: number;
+	constructor() {
+		this.signature = Math.random();
+	}
+	getSignature(): number {
+		return this.signature;
+	}
+}
 
+class Person {
+	private key: Key;
+	constructor(key: Key) {
+		this.key = key;
+	}
+
+	getKey() {
+		return this.key;
+	}
+}
+
+abstract class House {
+	protected door: boolean = false;
+	protected key: Key;
+	protected tenants: Person[] = [];
+	constructor(key: Key) {
+		this.key = key;
+	}
+
+	abstract openDoor(key: Key): void;
+
+	comeIn(person: Person) {
+		if (!this.door) return;
+		this.tenants.push(person);
+	}
+}
+
+class MyHouse extends House {
+	openDoor(key: Key) {
+		if (key.getSignature() === this.key.getSignature()) {
+			this.door = true;
+		}
+	}
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
